c61fbc34067977f11351bc5cd683c65e05e944c6
Maliseni1 <maliseni1205@gmail.com>
2025-11-20 09:00:58 +0200

    Fix: Temporariry commenting out the providers in main.jsx

--- nyumba-frontend/src/main.jsx
+++ nyumba-frontend/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+//import App from './App.jsx';
 import './index.css';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-geosearch/dist/geosearch.css';
 import { BrowserRouter } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { AuthContextProvider } from './context/AuthContext.jsx';
+//import { GoogleOAuthProvider } from '@react-oauth/google';
+//import { AuthContextProvider } from './context/AuthContext.jsx';
 // --- 1. IMPORT THE NEW THEME PROVIDER ---
-import { ThemeProvider } from './context/ThemeContext.jsx';
+//import { ThemeProvider } from './context/ThemeContext.jsx';
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
